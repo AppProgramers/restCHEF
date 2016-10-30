0054ee3f6264c82599100ec2ac32cf0de453441c
Manuel <manolos5535@hotmail.com>
2016-10-29 23:53:35 -0600

    resources

--- ChefServer/src/ce1103/ChefREST/resources/Chefrecipes.java
+++ ChefServer/src/ce1103/ChefREST/resources/Chefrecipes.java
@@ -1,13 +1,25 @@
 package ce1103.ChefREST.resources;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import ce1103.ChefREST.data.listwrapper;
+import ce1103.ChefREST.dataStructure.List;
 
 @Path("/Chef")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class Chefrecipes {
-
+	
+	@GET
+	@Path("/lista")
+	public List<String> sendRecipes(){
+		listwrapper.listaCarnes.insertHead("Manuel");
+		listwrapper.listaCarnes.insertHead("Alfonso");
+		listwrapper.listaCarnes.insertHead("Manuel");
+		return listwrapper.listaCarnes;
+	}
 }
